ca90f722f1863be171a7e6012dd5dbc9dd48eaca
jdjingdian <jdjingdian@gmail.com>
2025-12-24 10:59:00 +0800

    Initial Commit

---
+++ qdl/qdlApp.swift
@@ -0,0 +1,17 @@
+//
+//  qdlApp.swift
+//  qdl
+//
+//  Created by 经典 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct qdlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
